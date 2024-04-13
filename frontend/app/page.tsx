6736532c4e15850c3d4200dd4f29a35b262c7498
BlazzByte <nrwrnntemporal2@gmail.com>
2024-04-13 12:09:12 -0400

    init

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,11 +1,13 @@
 import Header from "@/app/components/header";
 import ChatSection from "./components/chat-section";
+import Footer from "./components/footer";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center gap-10 p-24 background-gradient">
+    <main className="flex min-h-screen flex-col items-center gap-4 p-24 background-gradient">
       <Header />
       <ChatSection />
+      <Footer />
     </main>
   );
 }
